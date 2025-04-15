771ad9e22c30779dd8c769c3a9a938de96c7af05
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2025-04-15 06:47:44 +0500

    Adding the logics

--- Day1.cpp
+++ Day1.cpp
@@ -120,3 +120,328 @@
 // return 0;
 // }
 
+//  * * * * *
+//  * * * * *
+//  * * * * *
+//  * * * * *
+//  * * * * *
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row =0; row < 5; row++){
+//     for(int col = 0; col<5; col++){
+//         cout<<" *";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+// 1 1 1 1 1 
+// 2 2 2 2 2 
+// 3 3 3 3 3 
+// 4 4 4 4 4 
+// 5 5 5 5 5 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row =1; row <= 5; row++){
+//     for(int col = 1; col<=5; col++){
+//         cout<<row<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+
+
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row =1; row <= 5; row++){
+//     for(int col = 1; col<=5; col++){
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int row = 1; row<=5; row++){
+//         for(int col = 5; col>=1; col--){
+//             cout<<col<<" ";
+//         }
+//         cout<<endl;
+//     }   
+// return 0;
+// }
+
+
+// 1 4 9 16 25 
+// 1 4 9 16 25 
+// 1 4 9 16 25 
+// 1 4 9 16 25 
+// 1 4 9 16 25 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int row = 1; row<=5; row++){
+//         for(int col = 1; col<=5; col++){
+//             cout<<col*col<<" ";
+//         }
+//         cout<<endl;
+//     }   
+// return 0;
+// }
+
+
+// a a a a a 
+// b b b b b 
+// c c c c c 
+// d d d d d 
+// e e e e e
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(char row = 'a'; row<= 'e'; row++){
+//     for(char col = 'a'; col<= 'e'; col++){
+//         cout<<row<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+
+// a b c d e 
+// a b c d e 
+// a b c d e 
+// a b c d e 
+// a b c d e 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(char row = 'a'; row<= 'e'; row++){
+//     for(char col = 'a'; col<= 'e'; col++){
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+// 1 2 3 4 5 
+// 6 7 8 9 10 
+// 11 12 13 14 15 
+// 16 17 18 19 20 
+// 21 22 23 24 25 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int count = 1;
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 1; col <= 5; col++){
+//         cout<<count<<" ";
+//         count++;
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+
+// ======================== Part 2 of pattern Printing 
+
+
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 1; col <=row; col++){
+//         cout<<" *";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int row = 1; row<=5; row++){
+//         for(int col = 1; col<=row; col++){
+//             cout<<col<<" ";
+//         }
+//         cout<<endl;
+//     }   
+// return 0;
+// }
+
+
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int row = 1; row<=5; row++){
+//         for(int col = 1; col<=row; col++){
+//             cout<<row<<" ";
+//         }
+//         cout<<endl;
+//     }   
+// return 0;
+// }
+
+
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 5; col >= row; col--){
+//         cout<<" *";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+// row  print 
+// 1    5
+// 2    4
+// 3    3
+// 4    2
+// 5    1
+
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 1; col <= (5 - row) + 1; col++){
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+
+// 5 
+// 5 4 
+// 5 4 3 
+// 5 4 3 2 
+// 5 4 3 2 1 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 1; col<=row; col++){
+//         cout<< (5 - col) + 1 <<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 1; col <= 5 - row + 1; col++){
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+//  }  
+// return 0;
+// }
+
+
+// part 3 of pattern printing ==============     
+
+
+//  - - - - - - - - - *
+//  - - - - - - - - * *
+//  - - - - - - - * * *
+//  - - - - - - * * * *
+//  - - - - - * * * * *
+//  - - - - * * * * * *
+//  - - - * * * * * * *
+//  - - * * * * * * * *
+//  - * * * * * * * * *
+//  * * * * * * * * * *
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  int n  = 10;
+//  for(int row = 1; row<=n; row++){
+//     for(int col = n - 1; col>=row; col--){
+//         cout<<" -";  // Here i use the - instead of the blank space for clear understanding
+//     }
+//     for(int col = 1; col<=row; col++){
+//         cout<<" *";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }

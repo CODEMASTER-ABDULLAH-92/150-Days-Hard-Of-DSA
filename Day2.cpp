7e30b9bdb49a4d17938c9900796417fb47d74773
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2025-04-30 19:19:38 +0500

    Removing the unwanted files

---
+++ Day2.cpp
@@ -0,0 +1,62 @@
+//          *
+//        * * *
+//      * * * * *
+//    * * * * * * *
+//  * * * * * * * * *
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the num: ";
+//     cin>>n;
+//  for(int row = 1; row<=n; row++){
+//     for(int col = n - 1; col>=row; col--){
+//         cout<<"  ";
+//     }
+//     for(int col = 1; col<= 2*row - 1; col++){
+//         cout<<" *";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+// Enter the num: 5
+//         1 
+//       1 2 1 
+//     1 2 3 2 1 
+//   1 2 3 4 3 2 1 
+// 1 2 3 4 5 4 3 2 1 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the num: ";
+//     cin>>n;
+//  for(int row = 1; row<= n; row++){
+//     for(int col = n - 1; col >= row; col--){
+//         cout<<"  ";
+//     }
+//     for(int col = 1; col<= row; col++){
+//         cout<<col<<" ";
+//     }
+//     for(int col = row; col>1; col--){
+//         cout<<col - 1<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int row = 1; row<= 5; row++){
+    }   
+return 0;
+}

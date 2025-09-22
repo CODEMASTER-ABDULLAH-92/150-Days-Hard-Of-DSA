632c5b498732b5e468cdfd0c09a35056dd46a40d
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-09-22 15:01:44 +0500

    Create MergeTwoSortedArrays.cpp

---
+++ newEraQuestions/Array/MergeTwoSortedArrays.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1;       // pointer for nums1 (last valid element)
+    int j = n - 1;       // pointer for nums2 (last element)
+    int k = m + n - 1;   // pointer for final position in nums1
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    // If nums2 still has elements left, copy them
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3, n = 3;
+
+    merge(nums1, m, nums2, n);
+
+    cout << "Merged Array: ";
+    for (int num : nums1) cout << num << " ";
+    return 0;
+}
